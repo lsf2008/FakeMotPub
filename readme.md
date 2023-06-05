7cc65f17501eacb273203cae30dab223ae22a43d
limax2008 <lishifeng2007@gmail.com>
2023-06-05 20:34:30 +0800

    update

---
+++ readme.md
@@ -0,0 +1,24 @@
+This is a PyTorch-lightning/GPU implementation of the paper 
+Video anomaly detection based on a motion pseudo-sample synthesizer.
+
+# Requirements
+To run the program, please install the following packages: 
+- pytorch==1.11.0
+- pytorch-lightning==1.9.0
+- pytorchvideo==0.1.5
+- opencv-python==4.7.0.68
+- PyYAML==6.0
+- prettytable==3.6.0
+- matplotlib==3.7.0
+- torchvision==0.12.0
+
+
+# How to run it`
+1. To start, prepare the data using a CSV file. Since the video is stored with images in a single folder, provide the folder name in the CSV file followed by the ground truth for each frame.
+2. Next, configure the YAML file using the data CSV file, and adjust other parameters such as batch size, learning rate, weight decay, and block size, etc.
+3. Finally, specify the "flg" in the [tst.py](http://tst.py/) file and run it.
+
+# Acknowledgement
+In our model implementation, we refer to the model implementation of the article "Latent Space Autoregression 
+for Novelty Detection", and we thank for the implementation of AE model.
+
--- tst.py
+++ tst.py
@@ -1,8 +1,6 @@
 # from aemodel.ae_multi_out import AeMultiOut
 import numpy as np
 import torch
-
-from aemodel.ae_multi_out_wghts import AeMultiOut
 from config.param_seting import initial_params
 from trainer.trainer import train_trainer,trainer_vd_module
 # from aemodel.autoencoder import convAE
@@ -11,7 +9,6 @@ from aemodel.ae_mlp import AeMlp, Ae2Mlps, Ae1Mlp2,Ae1Mlp3
 from trainer.fakeMot_cross_module import FakeMotCrossModule
 from trainer.fakeMot_noCls_module import FakeMotNoClsModule
 
-from trainer.mult_recLoss_module_finch import MultRecLossModuleFinch
 import pytorch_lightning as pl
 import prettytable as pt
 import time
@@ -41,7 +38,7 @@ if flg =='ped2':
     # mdl = MultAERecLossModule(model, **vars(args))
 
     # -----------------only motion-----------------
-    mdl = FakeMotNoClsModule(model, **vars(args))
+    mdl = FakeMotCrossModule(model, **vars(args))
 
     # 使用module训练模型
     res = trainer_vd_module(args, mdl, vd, True)
@@ -77,47 +74,3 @@ if flg =='ave':
     print(f'running time:{(end_time - stat_time) / 60} m')
 
 
-if flg =='shg':
-    tbl = pt.PrettyTable()
-    tbl.field_names = ['auc', 'cmb_coef', 'epoch', 'dataset']
-
-    args = initial_params('config/dtsht_cfg.yml')
-    stat_time = time.time()
-
-    trn_pth = 'E:/dataset/shanghaitech/label/remove_bg/train/'
-    trnList = utils.getImgListFrm1Flder(trn_pth, type='*.csv')
-
-    tstPth = 'E:/dataset/shanghaitech/label/remove_bg/test/'
-    aucList = []
-    for trn in trnList:
-        tstCSV = tstPth + 'test' + str(trn).split('_')[-1]
-
-        args['train_dataPath'] = str(trn)
-        args['val_dataPath'] = tstCSV
-
-        # 数据集
-        vd = VideoDataLoader(**vars(args))
-
-        # 模型
-        model = AeMultiOut(input_shape=args.input_shape,
-                           code_length=args.code_length,
-                           layers=args.wght_layers)
-        # trainer module
-        mdl = MultAeMotRecLossModule(model, **vars(args))
-
-        # 使用module训练模型
-        res = trainer_vd_module(args, mdl, vd)
-
-        # print(str(trn)+res['maxAuc'])
-        aucList.append(res['maxAuc'])
-        sn = 'data/shg/' + str(trn).split('_')[-1].split('.')[0] + '.pt'
-        tbl.add_row([res['maxAuc'], res['coef'], res['epoch'], str(trn).split('_')[-1].split('.')[0]])
-        torch.save(res, sn)
-
-    end_time = time.time()
-
-    print(tbl)
-    print(f'running time:{(end_time - stat_time) / 60} m')
-    sv_name = 'data/shg/shg_wgtLayer[034]_multLayer034.txt'
-    with open(sv_name, 'w') as f:
-        f.write(tbl.get_string())
