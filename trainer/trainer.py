9a4067274fa0d3292a1c407603936b10d6b8fa0b
lsf2008 <limax_2008@outlook.com>
2023-06-06 19:48:10 +0800

    update

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,20 +1,8 @@
 
 from dataset.video_dataloader import VideoDataLoader
-from trainer.mult_ae_recLoss_module import MultAERecLossModule
 import pytorch_lightning as pl
 import time
 
-def train_trainer(args, model):
-    vd = VideoDataLoader(**vars(args))
-    train_dl = vd.train_dataloader()
-    val_dl = vd.val_dataloader()
-
-    # inmd = model(**vars(args))
-    mdl = MultAERecLossModule(model, **vars(args))
-    trainer = pl.Trainer.from_argparse_args(args)
-    trainer.fit(mdl, train_dataloaders=train_dl, val_dataloaders=val_dl)
-    # trainer.test(dataloaders=val_dl, ckpt_path='best')
-    return mdl.res
 
 def trainer_vd_module(args, module, vd, is_test=False):
     # vd = VideoDataLoader(**vars(args))
